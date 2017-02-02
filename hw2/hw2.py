6b12a4d69ea821d95c6d27d49d64e0732fe9796b
Caleb Zulawski <caleb.zulawski@gmail.com>
2017-02-01 23:29:28 -0500

    Switch to softmax output

--- hw2/hw2.py
+++ hw2/hw2.py
@@ -30,7 +30,7 @@ class Model():
         n_hidden_1 = 20
         n_hidden_2 = 20
         n_input = 2
-        n_output = 1
+        n_output = 2
 
         self.x = tf.placeholder(tf.float32, [n_input, 1])
         self.y = tf.placeholder(tf.float32, [1, n_output])
@@ -48,23 +48,22 @@ class Model():
         layer_2 = tf.add(tf.matmul(layer_1, w2), b2)
         layer_2 = tf.nn.relu(layer_2)
 
-        self.out = tf.matmul(layer_2, wo) + bo
+        self.layer_out = tf.matmul(layer_2, wo) + bo
+        self.out = tf.nn.softmax(self.layer_out)
 
     def train(self, xs, ys, cs):
-        two = tf.constant(2, dtype=tf.float32)
-        cost = tf.reduce_sum(tf.divide(tf.squared_difference(self.y, self.out), two))
-        # cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=self.out, labels=self.y))
+        cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=self.layer_out, labels=self.y))
         optimizer = tf.train.AdamOptimizer(learning_rate=self.learning_rate).minimize(cost)
         self.sess.run(tf.global_variables_initializer())
         for epoch in range(self.n_epochs):
             for x, y, c in zip(xs, ys, cs):
                 v = np.expand_dims(np.asarray([x, y]), axis=1)
-                l = np.expand_dims(np.asarray([c]), axis=1)
+                l = np.reshape([not(c).astype(float), c.astype(float)], (1, 2))
                 self.sess.run(optimizer, feed_dict={self.x: v, self.y: l})
 
     def predict(self, x, y):
         v = np.expand_dims(np.asarray([x, y]), axis=1)
-        return self.sess.run(self.out, feed_dict={self.x: v})[0][0] > 0.5
+        return self.sess.run(self.out, feed_dict={self.x: v})[0][1] > 0.5
 
 n_train = 100;
 n_test = 1000;
@@ -72,7 +71,7 @@ x_train, y_train, c_train = get_data(n_train)
 x_test, y_test, c_test = get_data(n_test)
 
 with tf.Session() as sess:
-    model = Model(sess, 100, 0.01)
+    model = Model(sess, 200, 0.01)
     model.train(x_train, y_train, c_train)
 
     total = 0;
