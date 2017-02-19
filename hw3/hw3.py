376efa07b4730b1c47e69593b870c78534af509c
Caleb Zulawski <caleb.zulawski@gmail.com>
2017-02-19 11:49:34 -0500

    Finish hw3

--- hw3/hw3.py
+++ hw3/hw3.py
@@ -18,7 +18,6 @@ class Model():
     def build_model(self):
         self.inputs = tf.placeholder(tf.float32, [None, self.pixels, self.pixels, 1])
         self.labels = tf.placeholder(tf.float32, [None, self.n_classes])
-        print(self.inputs)
         with slim.arg_scope([slim.conv2d, slim.fully_connected],
                             activation_fn=tf.nn.relu,
                             weights_initializer=tf.truncated_normal_initializer(0.0, 0.01),
@@ -28,10 +27,10 @@ class Model():
             net = slim.max_pool2d(net, [2, 2], scope='pool1')
             net = slim.repeat(net, 1, slim.conv2d, 64, [5, 5], scope='conv2')
             net = slim.max_pool2d(net, [2, 2], scope='pool2')
-            net = slim.conv2d(net, 1024, [7, 7], padding='VALID', scope='fc3')
-            net = slim.dropout(net, 0.5, scope='dropout3')
-            net = slim.conv2d(net, self.n_classes, [1, 1], activation_fn=None, normalizer_fn=None, scope='fc4')
-            net = tf.squeeze(net, [1, 2], name='fc4/squeezed')
+            net = slim.flatten(net, scope='flatten2')
+            net = slim.fully_connected(net, 1024, scope='fc3')
+            net = slim.dropout(net, 0.8, scope='dropout3')
+            net = slim.fully_connected(net, self.n_classes, activation_fn=None, normalizer_fn=None, scope='fc4')
         self.predictions = net
         slim.losses.softmax_cross_entropy(self.predictions, self.labels)
         self.loss = slim.losses.get_total_loss(add_regularization_losses=True)
@@ -43,13 +42,17 @@ class Model():
     def train_minibatch(self, xs, ys):
         self.sess.run(self.optimizer, feed_dict={self.inputs: xs, self.labels: ys})
 
-    def train(self, xs, ys):
+    def train(self, xs, ys, xvs, yvs, epochs):
         num_minibatches = xs.shape[0] // self.n_batch
-        for i in range(num_minibatches):
-            print("Minibatch " + str(i) + '/' + str(num_minibatches))
-            start = i * self.n_batch
-            end = (i + 1) * self.n_batch
-            self.train_minibatch(xs[start:end], ys[start:end])
+        for epoch in range(epochs):
+            p = np.random.permutation(xs.shape[0])
+            for i in range(num_minibatches):
+                # print("Minibatch " + str(i) + '/' + str(num_minibatches))
+                start = i * self.n_batch
+                end = (i + 1) * self.n_batch
+                self.train_minibatch(xs[p][start:end], ys[p][start:end])
+            accuracy = self.validate(xvs, yvs)
+            print("Epoch {} validation accuracy: {}%".format(epoch, accuracy))
 
     def validate(self, xs, ys):
         return self.sess.run(self.accuracy, feed_dict={self.inputs: xs, self.labels: ys})
@@ -59,7 +62,8 @@ class Model():
 
 with tf.Session() as sess:
     data = mnist.read_data_sets("MNIST_data/", dtype=tf.uint8, reshape=False, one_hot=True)
-    m = Model(sess, 28, 10, 10, 0.01)
+    m = Model(sess, 28, 50, 10, 0.001)
     sess.run(tf.global_variables_initializer())
-    m.train(data.train.images, data.train.labels)
-    print(m.validate(data.validation.images, data.validation.labels))
+    m.train(data.train.images, data.train.labels, data.validation.images, data.validation.labels, 2)
+    test_accuracy = m.validate(data.test.images, data.test.labels)
+    print("Test set accuracy: {}".format(test_accuracy))
